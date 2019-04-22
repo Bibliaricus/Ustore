6eb43cf8fcceb33bbf5fb45adbb49cda1fbe3a84
Bibliaricus <slavikdev95@gmail.com>
2019-04-22 15:45:14 +0300

    Add quick view popup from previous project

--- js/script.js
+++ js/script.js
@@ -52,3 +52,23 @@
       div.removeClass('form-retention');
     } 
   });
+
+  // Quantity of products in quick view popup
+  (function quantityProducts() {
+    var $quantityArrowMinus = $(".quantity__minus");
+    var $quantityArrowPlus = $(".quantity__plus");
+    var $quantityNum = $(".quantity__number-input");
+ 
+    $quantityArrowMinus.click(quantityMinus);
+    $quantityArrowPlus.click(quantityPlus);
+ 
+    function quantityMinus() {
+      if ($quantityNum.val() > 1) {
+        $quantityNum.val(+$quantityNum.val() - 1);
+      }
+    }
+ 
+    function quantityPlus() {
+      $quantityNum.val(+$quantityNum.val() + 1);
+    }
+  })();
