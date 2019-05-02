da822b28e880c79c0ab2b1bd5397a0b2160ca8ac
Bibliaricus <slavikdev95@gmail.com>
2019-05-02 10:35:48 +0300

    Creates markup for footer

--- index.php
+++ index.php
@@ -1085,6 +1085,86 @@
       </div>
     </div>
   </div>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="footer__contacts col-3">
+          <img src="img/footer-logo.jpg" alt="Footer logo" class="footer__logo">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod lorem.</p>
+          <span>48 Park Avenue,</span>
+          <span>East 21st Street, Apt. 304</span>
+          <span>New York NY 10016</span>
+          <span>Email: <a href="mailto:youremail@site.com" class="contact-link">youremail@site.com</a></span>
+          <span>Phone: <a href="tel:+14089961010" class="contact-link">+1 408 996 1010</a></span></span>
+        </div>
+        <div class="footer__links col-3">
+          <h4>Useful Links</h4>
+          <div class="footer__links-cont">
+            <ul>
+              <li><a href="#">Home Page</a></li>
+              <li><a href="#">About Us</a></li>
+              <li><a href="#">Delivery Info</a></li>
+              <li><a href="#">Conditions</a></li>
+              <li><a href="#">Order Tracking</a></li>
+              <li><a href="#">My Account</a></li>
+              <li><a href="#">My Wishlist</a></li>
+            </ul>
+            <ul>
+              <li><a href="#">London</a></li>
+              <li><a href="#">San Fransisco</a></li>
+              <li><a href="#">New Orlean</a></li>
+              <li><a href="#">Seatle</a></li>
+              <li><a href="#">Portland</a></li>
+              <li><a href="#">Stockholm</a></li>
+              <li><a href="#">Hoffenheim</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer__posts col-3">
+          <h4>Latest posts</h4>
+          <ul>
+            <li>
+              <img src="img/latest_news-1.jpg" alt="Image to latest posts">
+              <div class="posts__text">
+                <h5 class="posts__title">Down The Road</h5>
+                <time class="icon-font-baseline-today-24px">November 24, 2013</time>
+                <span class="icon-font-bubble">5 Comments</span>
+              </div>
+            </li>
+            <li>
+              <img src="img/latest_news-2.jpg" alt="Image to latest posts">
+              <div class="posts__text">
+                <h5 class="posts__title">Down The Road</h5>
+                <time class="icon-font-baseline-today-24px">November 24, 2013</time>
+                <span class="icon-font-bubble">5 Comments</span>
+              </div>
+            </li>
+            <li>
+              <img src="img/latest_news-3.jpg" alt="Image to latest posts">
+              <div class="posts__text">
+                <h5 class="posts__title">Down The Road</h5>
+                <time class="icon-font-baseline-today-24px">November 24, 2013</time>
+                <span class="icon-font-bubble">5 Comments</span>
+              </div>
+            </li>
+          </ul>
+        </div>
+        <div class="footer__tags col-3">
+          <a href="#">Illegal</a>
+          <a href="#">Accesories</a>
+          <a href="#">Sale</a>
+          <a href="#">Jeans</a>
+          <a href="#">Fashion</a>
+          <a href="#">Illegal</a>
+          <a href="#">Accesories</a>
+          <a href="#">Sale</a>
+          <a href="#">Jeans</a>
+          <a href="#">Fashion</a>
+          <a href="#">Accesories</a>
+        </div>
+      </div>
+    </div>
+  </footer>
 
   <!-- Modals -->
 
