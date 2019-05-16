e5ce2c71425d67ab0a901eee1953515cb193124a
Bibliaricus <slavikdev95@gmail.com>
2019-05-16 16:18:30 +0300

    Link to DB in localhost

--- bd.php
+++ bd.php
@@ -1,3 +1,9 @@
 <?php
-  $db = mysqli_connect("localhost", "cq33394_users", "2CyzpkF1", "cq33394_users");
+  /**
+   * "localhost" - your MySQL server
+   * "root" - login to this server (writes in admin panel in hostings or default login to phpMyAdmin)
+   * "" - password to this server (in hosting admin panel or password to phpMyAdmin)
+   * "Ustore" - name of data base
+   */
+  $db = mysqli_connect("localhost", "root", "", "Ustore");
 ?> 
