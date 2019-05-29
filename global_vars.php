2fecae60322d9d8a6fb2e77d4fb63ee78c7a7af2
Bibliaricus <slavikdev95@gmail.com>
2019-05-29 15:09:50 +0300

    Create file with variables, which contain paths to template files

---
+++ global_vars.php
@@ -0,0 +1,8 @@
+<?php 
+  $html_head = 'templates/html_head.php';
+  $header = 'templates/header.php';
+  $footer = 'templates/footer.php';
+  $quick_view_popup = 'templates/modals/quick_view_popup.php';
+  $subscribe_modal = 'templates/modals/subscribe_modal.php';
+  $functions = 'templates/function.php';
+?>
