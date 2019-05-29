03660c891b31b3eaa8c44a45544743f0f4dd8f97
Bibliaricus <slavikdev95@gmail.com>
2019-05-29 15:19:15 +0300

    Add to function.php closing tag </html>

--- templates/function.php
+++ templates/function.php
@@ -1,8 +1,9 @@
 <?php ?>
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/slick.min.js"></script>
-  <script src="js/script.js"></script>
-</body>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
 <?php ?>
