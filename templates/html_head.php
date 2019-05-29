b3e3baec324452992b18060933229bed81088a73
Bibliaricus <slavikdev95@gmail.com>
2019-05-29 15:20:01 +0300

    Add doctype and open tag <html> to html_head.php

--- templates/html_head.php
+++ templates/html_head.php
@@ -1,22 +1,24 @@
 <?php ?>
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-<!-- Favicons -->
-<link rel="icon" href="favicon-16x16.png" sizes="16x16" type="image/png">
-<link rel="icon" href="favicon-32x32.png" sizes="32x32" type="image/png">
-<link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
-<link rel="icon" href="android-chrome-192x192.png" sizes="192x192" type="image/png">
-<link rel="manifest" href="site.webmanifest">
+    <!-- Favicons -->
+    <link rel="icon" href="favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="icon" href="favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+    <link rel="icon" href="android-chrome-192x192.png" sizes="192x192" type="image/png">
+    <link rel="manifest" href="site.webmanifest">
 
-<title>Ustore - pretty smart shop.</title>
+    <title>Ustore - pretty smart shop.</title>
 
-<link rel="stylesheet" href="css/normalize.css">
-<link rel="stylesheet" href="css/bootstrap.min.css">
-<link rel="stylesheet" href="css/animate.min.css">
-<link rel="stylesheet" href="css/slick.css">
-<link rel="stylesheet" href="css/style.min.css">
-</head>
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/style.min.css">
+  </head>
 <?php ?>
