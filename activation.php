b8434e7f2a0ce2365ede4106d4c23e0f8b3dc800
Bibliaricus <slavikdev95@gmail.com>
2019-06-03 21:11:41 +0300

    Rewrite activation.php with new style

--- activation.php
+++ activation.php
@@ -1,4 +1,5 @@
 <?php
+  include "error-page.php";
   include "bd.php"; // файл bd.php должен быть в той же папке, что и все остальные, если это не так, то просто    измените путь
   $result4 = mysqli_query($db, "SELECT avatar FROM users WHERE activation='0' AND UNIX_TIMESTAMP() - UNIX_TIMESTAMP(date) > 3600"); //извлекаем аватарки тех пользователей, которые в    течении часа не активировали свой аккаунт. Следовательно их надо удалить из    базы, а так же и файлы их аватарок
   if (mysqli_num_rows($result4) > 0) {
@@ -15,19 +16,40 @@
   if (isset($_GET['code'])) {
     $code = $_GET['code']; //код подтверждения
   } else {
-    exit("Вы зашли на страницу без кода подтверждения!");  //если не указали code,    то выдаем ошибку
+    echo $errorPageContent_Start;
+?>
+      <p>You have visited the page without a verification code!</p>
+<?php
+echo $errorPageContent_End;
+    
+    exit(footerInErrorPage());  //если не указали code,    то выдаем ошибку
   }
   if (isset($_GET['login'])) {
     $login = $_GET['login'];  //логин,который    нужно активировать
   } else {
-    exit("Вы зашли на страницу без логина!");  //если не указали логин, то выдаем ошибку
+    echo $errorPageContent_Start;
+    ?>
+    <p>You have visited the page without a login!</p>
+    <?php
+    echo $errorPageContent_End;
+    exit(footerInErrorPage());
   }
   $result = mysqli_query($db, "SELECT id FROM users WHERE login='$login'"); //извлекаем    идентификатор пользователя с данным логином
   $myrow = mysqli_fetch_array($result);
   $activation = md5($myrow['id']) . md5($login); //создаем    такой же код подтверждения
   if ($activation == $code) { //сравниваем полученный из url и сгенерированный код
       mysqli_query($db, "UPDATE    users SET activation='1' WHERE login='$login'"); //если равны, то активируем пользователя
-      echo "Ваш Е-мейл подтвержден! Теперь вы можете    зайти на сайт под своим логином! <a href='index.php'>Главная    страница</a>";
+      echo $successPageContent_Start;
+      ?>
+      <p>Your email is confirmed! Now you can enter the site under your login!</p>
+      <?php
+      echo $errorPageContent_End;
   } else {
-    echo "Ошибка! Ваш Е-мейл не  подтвержден! <a    href='index.php'>Главная    страница</a>"; //если    же полученный из url и    сгенерированный код не равны, то выдаем ошибку
+    echo $errorPageContent_Start
+    ?>
+    <p>Mistake! Your email is not confirmed!</p>
+    <?php
+    echo $errorPageContent_End;
   }
+  footerInErrorPage();
+?>
