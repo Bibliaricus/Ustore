80a91a7bfb97ef78554ed348a1c59632813736d0
Bibliaricus <slavikdev95@gmail.com>
2019-06-04 10:56:16 +0300

    Delete russion comments from exit.php

--- exit.php
+++ exit.php
@@ -2,7 +2,6 @@
 session_start();
 if (empty($_SESSION['login']) or empty($_SESSION['password'])) {
     include "error-page.php";
-    //если не существует сессии с логином и паролем, значит на    этот файл попал невошедший пользователь. Ему тут не место. Выдаем сообщение    об ошибке, останавливаем скрипт
     echo $errorPageContent_Start;
     ?>
     <p>Access to this page is allowed only to registered users. If you are registered, log in to the site with your username and password</p>
@@ -18,6 +17,5 @@ $test = mysqli_query($db, "UPDATE users SET exit_time='$nowTimestamp' WHERE logi
 
 unset($_SESSION['password']);
 unset($_SESSION['login']);
-unset($_SESSION['id']); //    уничтожаем переменные в сессиях
+unset($_SESSION['id']);
 exit("<html><head><meta    http-equiv='Refresh' content='2;    URL=index.php'></head></html>");
-// отправляем пользователя на главную страницу.
