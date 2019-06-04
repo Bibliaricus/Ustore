f85b38104eada1bc871a89480957cfd09c986069
Bibliaricus <slavikdev95@gmail.com>
2019-06-04 12:05:07 +0300

    Delete russion comments from post.php

--- post.php
+++ post.php
@@ -1,14 +1,12 @@
 <?php
-session_start(); //запускаем сессию. Обязательно в начале страницы
-include "bd.php"; // соединяемся с базой, укажите свой путь, если у вас    уже есть соединение
+session_start();
+include "bd.php";
 if (!empty($_SESSION['login']) and !empty($_SESSION['password'])) {
-    //если    существует логин и пароль в сессиях, то проверяем, действительны ли они
     $login = $_SESSION['login'];
     $password = $_SESSION['password'];
     $result2 = mysqli_query($db, "SELECT id,avatar FROM users WHERE login='$login' AND password='$password'");
     $user = mysqli_fetch_array($result2);
     if (empty($user['id'])) {
-        //если логин    или пароль не действителен
         include "error-page.php";
         echo $errorPageContent_Start;
         ?>
@@ -25,13 +23,12 @@ if (!empty($_SESSION['login']) and !empty($_SESSION['password'])) {
     <?php
     echo $errorPageContent_End;
     exit(footerInErrorPage());}
-if (isset($_POST['id'])) {$id = $_POST['id'];} //получаем идентификатор страницы    получателя
-if (isset($_POST['message-to-user'])) {$text = $_POST['message-to-user'];} //получаем текст сообщения
-if (isset($_POST['recipient'])) {$poluchatel = $_POST['recipient'];} //логин получателя
-$author = $_SESSION['login']; //логин автора
-// echo $text;
-$date = date("Y-m-d"); //дата добавления
-if (empty($author) or empty($text) or empty($poluchatel) or empty($date)) { //есть ли все необходимые    данные? Если нет, то останавливаем
+if (isset($_POST['id'])) {$id = $_POST['id'];}
+if (isset($_POST['message-to-user'])) {$text = $_POST['message-to-user'];}
+if (isset($_POST['recipient'])) {$poluchatel = $_POST['recipient'];}
+$author = $_SESSION['login'];
+$date = date("Y-m-d");
+if (empty($author) or empty($text) or empty($poluchatel) or empty($date)) {
     include "error-page.php";
     echo $errorPageContent_Start;
     ?>
@@ -39,10 +36,10 @@ if (empty($author) or empty($text) or empty($poluchatel) or empty($date)) { //е
     <?php
     errorPageContent_End();  
     exit(footerInErrorPage());}    
-$text = stripslashes($text); //удаляем обратные слеши
-$text = htmlspecialchars($text); //преобразование    спецсимволов в их HTML эквиваленты
+$text = stripslashes($text);
+$text = htmlspecialchars($text);
 
-$result2 = mysqli_query($db, "INSERT INTO    messages (author, recipient, date, message) VALUES    ('$author','$poluchatel','$date','$text')"); //заносим в базу сообщение
+$result2 = mysqli_query($db, "INSERT INTO    messages (author, recipient, date, message) VALUES    ('$author','$poluchatel','$date','$text')");
 include "error-page.php";
 echo $successPageContent_Start;
 ?>
