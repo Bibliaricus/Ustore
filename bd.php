c56f282d525d69a1f9dffa6c55ef87a87760981f
Bibliaricus <slavikdev95@gmail.com>
2019-06-05 11:02:11 +0300

    Add query to connect site with DB in the server

--- bd.php
+++ bd.php
@@ -5,5 +5,11 @@
    * "" - password to this server (in hosting admin panel or password to phpMyAdmin)
    * "Ustore" - name of data base
    */
+
+  //  For servers
+  // Arguments: 1 - acces poin, 2 - user login of DB, 3 - user password of DB, 4 - name of DB
+  // $db = mysqli_connect("localhost", "cs90695_ustore", "24019950dn", "cs90695_ustore");
+
+  // For locals
   $db = mysqli_connect("localhost", "root", "", "Ustore");
 ?>
